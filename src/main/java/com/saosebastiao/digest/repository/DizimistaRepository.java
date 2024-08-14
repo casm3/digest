44ff9b314efca2d97644355fbf1e5bb2267974a7
Carlos Melo <carlosmelo@hotmail.com.br>
2024-08-14 15:24:28 -0300

    Service de Dizimista

--- src/main/java/com/saosebastiao/digest/repository/DizimistaRepository.java
+++ src/main/java/com/saosebastiao/digest/repository/DizimistaRepository.java
@@ -4,6 +4,11 @@ import com.saosebastiao.digest.entity.Dizimista;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DizimistaRepository extends JpaRepository<Dizimista, Long> {
+  List<Dizimista> findByNomeContaining(String nome);
+  Optional<Dizimista> findByCpf(String cpf);
 }
---
+++ src/main/java/com/saosebastiao/digest/service/DizimistaService.java
@@ -0,0 +1,40 @@
+package com.saosebastiao.digest.service;
+
+import com.saosebastiao.digest.entity.Dizimista;
+import com.saosebastiao.digest.repository.DizimistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DizimistaService {
+
+  @Autowired
+  private DizimistaRepository dizimistaRepository;
+
+  public Dizimista salvarDizimista(Dizimista dizimista) {
+    return dizimistaRepository.save(dizimista);
+  }
+
+  public List<Dizimista> listarDizimistas() {
+    return dizimistaRepository.findAll();
+  }
+
+  public Optional<Dizimista> buscarPorId(Long id) {
+    return dizimistaRepository.findById(id);
+  }
+
+  public void excluirDizimista(Long id) {
+    dizimistaRepository.deleteById(id);
+  }
+
+  public List<Dizimista> buscarPorNome(String nome) {
+    return dizimistaRepository.findByNomeContaining(nome);
+  }
+
+  public Optional<Dizimista> buscarPorCpf(String cpf) {
+    return dizimistaRepository.findByCpf(cpf);
+  }
+}
